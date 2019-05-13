5d1b67eccaf8b0d039022012c6e5036e23060144
sjyjytu <1332372004@qq.com>
2019-05-13 14:09:36 +0800

    Updates

--- precache-manifest.3d8074985c869733ad9480c238e6fc21.js
+++ precache-manifest.3d8074985c869733ad9480c238e6fc21.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/JBook/static/js/runtime~main.2d8bf87d.js"
   },
   {
-    "revision": "a7baab9f45d37e9f591a",
-    "url": "/JBook/static/js/main.a7baab9f.chunk.js"
+    "revision": "8a835c3859573561ec11",
+    "url": "/JBook/static/js/main.8a835c38.chunk.js"
   },
   {
     "revision": "382dc929f9ae71dd1e9c",
     "url": "/JBook/static/js/2.382dc929.chunk.js"
   },
   {
-    "revision": "6d2b902a8fa26cc327f1ed2f3dcd7e62",
+    "revision": "56e84e01580dbdbfaf38e3d364bf2b2a",
     "url": "/JBook/index.html"
   }
 ];
